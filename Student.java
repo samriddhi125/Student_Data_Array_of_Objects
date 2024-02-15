9a69cbce20215c18c73371b0752060c21faaee38
samriddhi125 <114855641+samriddhi125@users.noreply.github.com>
2024-02-15 17:07:03 +0530

    Student class

---
+++ Student.java
@@ -0,0 +1,33 @@
+//
+class Student {
+	private String name;
+	private int prn;
+	private String dob;
+
+	//Setter and Getter methods for NAME
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	//Setter and Getter methods for PRN
+	public void setPrn(int prn){
+		this.prn = prn;
+	}
+
+	public int getPrn(){
+		return prn;
+	}
+
+	//Setter and Getter methods for DOB
+	public void setDob(String dob){
+		this.dob = dob;
+	}
+
+	public String getDob(){
+		return dob;
+	}
+}
